b46024bb17a70b01b805a506de6994c92075f82b
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2022-11-07 00:45:48 +0100

    Add aliases and types
    Aliases for schema entities. Types for sign up, register and chores specs use

--- lib/chorer.ex
+++ lib/chorer.ex
@@ -2,7 +2,33 @@ defmodule Chorer do
   import Ecto.Changeset
   import Ecto.Query
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias Chorer.Repo
+  alias ChorerSchemas.{Account, AccountFriend, Chore, Score, Token}
+  alias VBT.Accounts
+
+  @type sign_up_params :: %{
+          :email => String.t(),
+          :password => String.t(),
+          :first_name => String.t(),
+          :last_name => String.t()
+        }
+
+  @type register_params :: %{
+          :email => String.t(),
+          :password => String.t(),
+          :first_name => String.t(),
+          :last_name => String.t()
+        }
+
+  @type chore_params :: %{
+          :title => String.t(),
+          :description => String.t(),
+          :state => Chore.State.t(),
+          :frequency => integer(),
+          :repeating? => boolean(),
+          :duration => integer(),
+          :mental_difficulty => integer(),
+          :physical_difficulty => integer()
+        }
+
 end
