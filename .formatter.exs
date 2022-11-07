480d180b5483fad54befefd6821849d262ef93dc
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2022-11-07 01:28:20 +0100

    Configure surface formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  import_deps: [:absinthe, :ecto, :ecto_enum, :ecto_sql, :phoenix, :surface],
+  inputs: [
+    "lib/**/*.{ex,sface}",
+    "priv/*/seeds.exs",
+    "priv/*/migrations/*.exs",
+    "{config,lib,test}/**/*.{ex,exs}"
+  ],
+  plugins: [Surface.Formatter.Plugin]
 ]
