dd0213167d3d65c3e874781e8f126373cd58dba8
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2022-11-07 01:15:25 +0100

    Add components
    Flash, inputs, button. Scores chart

---
+++ lib/chorer_web/common_components/description.ex
@@ -0,0 +1,59 @@
+# credo:disable-for-this-file VBT.Credo.Check.Consistency.FileLocation
+defmodule ChorerWeb.CommonComponents.DescriptionList do
+  use ChorerWeb, :component
+
+  slot default
+
+  @impl Surface.Component
+  def render(assigns) do
+    ~F"""
+    <div class="w-full px-6">
+      <dl class="divide-y divide-gray-200">
+        <#slot />
+      </dl>
+    </div>
+    """
+  end
+end
+
+defmodule ChorerWeb.CommonComponents.DescriptionFlatRow do
+  use ChorerWeb, :component
+
+  prop name, :string, required: true
+  prop value, :string, required: true
+
+  @impl Surface.Component
+  def render(assigns) do
+    ~F"""
+    <div class="flex flex-row py-5">
+      <dt class="flex-1 text-sm font-bold text-gray-500">
+        {@name}
+      </dt>
+      <dd class="flex-1 mt-1 text-sm text-gray-900">
+        {@value}
+      </dd>
+    </div>
+    """
+  end
+end
+
+defmodule ChorerWeb.CommonComponents.DescriptionStackRow do
+  use ChorerWeb, :component
+
+  prop name, :string, required: true
+  prop value, :string, required: true
+
+  @impl Surface.Component
+  def render(assigns) do
+    ~F"""
+    <div class="py-5">
+      <dt class="text-sm font-bold text-gray-500">
+        {@name}
+      </dt>
+      <dd class="mt-1 text-sm text-gray-900">
+        {@value}
+      </dd>
+    </div>
+    """
+  end
+end
