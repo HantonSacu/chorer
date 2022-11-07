4c88261e5e002bee837919f5b3e08b4a6412df86
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2022-11-07 01:30:12 +0100

    Configure tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,14 @@ config :chorer, ChorerWeb.Endpoint,
   secret_key_base: "RJlAz62LuYNhFh8nePyIdKkSnI8996k4pvn7IcmNsgzsp6rEGeM4vnablVZjwhG4",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
